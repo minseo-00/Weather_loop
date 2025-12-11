f9a28d6ee14cbf90201896e80e0702d6c4cc730c
limminseo <limminsoe08@gmail.com>
2025-12-11 21:56:24 +0900

    ✨ Feat:날씨 API 연동 및 헤더에 실시간 날씨/아이콘/온도 표시 (#15)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
-import ClientRoot from "./client-root";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ClientRoot>{children}</ClientRoot>
+        {children}
       </body>
     </html>
   );
